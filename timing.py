6e5ae5cba9db2eaa13dc2a44f39eaccf4a2c2bec
Mohib Khan <mohibkhan949@gmail.com>
2021-03-06 13:26:19 +0530

    Change1

--- timing.py
+++ timing.py
@@ -4,4 +4,4 @@ def calculate_time(function):
     timebefore = time.time()
     function()
     timeafter = time.time()
-    print(f"Total time {timeafter - timebefore}")
+    print(f'Total time {timeafter - timebefore}')
