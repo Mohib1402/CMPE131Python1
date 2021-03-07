40227a374f1c1a84dc87a0bd27ef59a9758de69d
Mohib Khan <mohibkhan949@gmail.com>
2021-03-07 12:39:07 +0530

    Change2 (investment) Using isinstance method for checking conditions in if statement

--- investment.py
+++ investment.py
@@ -1,12 +1,12 @@
 def calculate_apr(principal, interest_rate, years):
-
-	try:
-		principal =float(principal)
-		interest_rate = float(interest_rate)
-		years = int(years)
-		for i in range(years):
-			principal =  principal * (1 + interest_rate)
+	if principal >= 0 and interest_rate >= 0 and years >= 0
+		p = principal
+		i = interest_rate
+		y = years
+		if((isinstance(p,float) or isinstance(p, int)) and (isinstance(i,float) or isinstance(i, int)) and isinstance(y, int))
+			for i in range(years):
+				principal =  principal * (1 + interest_rate)
 			return f'{principal}'
 
-	except ValueError:
+	else:
 		return False;
